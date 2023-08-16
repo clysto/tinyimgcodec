fc3ff1ade5a99b220488eab4e7ab5b19eb556c36
毛亚琛 <maoyachen55@gmail.com>
2023-08-16 13:07:32 +0800

    change build script

--- c/build-archive.sh
+++ c/build-archive.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 tar czf tinyimgcodec.tar.gz img.c img.h fifo.c fifo.h
-echo "sed '1,/^#EOF#$/d' \"\$0\" | tar zx; exit 0" > tinyimgcodec.sh
-echo "#EOF#" >> tinyimgcodec.sh
-cat tinyimgcodec.tar.gz >> tinyimgcodec.sh
+echo -n "DATA=\"" > tinyimgcodec.sh
+cat tinyimgcodec.tar.gz | base64 | tr -d '\n\r' >> tinyimgcodec.sh
+echo "\"" >> tinyimgcodec.sh
+echo "echo \"\$DATA\" | base64 --decode | tar zx" >> tinyimgcodec.sh
 rm tinyimgcodec.tar.gz
