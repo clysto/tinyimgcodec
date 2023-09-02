8dc8ae3c5d753faf4d21f78a26450ad6b2c51ee7
毛亚琛 <maoyachen55@gmail.com>
2023-09-02 13:32:07 +0800

    enhance viwer.py

--- viewer.py
+++ viewer.py
@@ -2,10 +2,23 @@
 
 import pathlib
 import sys
+from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 from tinyimgcodec import decompress
+
+
+def open_file(filename):
+    s = str.lower(Path(filename).suffix)
+    if s == ".img":
+        f = open(filename, "rb")
+        return decompress(f.read())
+    else:
+        return np.asarray(Image.open(filename).convert("L"), dtype=np.uint8)
+
 
 nimg = len(sys.argv) - 1
 
@@ -21,7 +34,7 @@ else:
 nrow = (nimg + 1) // 2
 
 fig, axs = plt.subplots(nrow, ncol)
-fig.canvas.manager.set_window_title('Viewer')
+fig.canvas.manager.set_window_title("Viewer")
 
 if nimg == 1:
     axs = [axs]
@@ -29,12 +42,11 @@ else:
     axs = axs.flatten()
 
 for i in range(nimg):
-    with open(sys.argv[i + 1], "rb") as f:
-        im = decompress(f.read())
-        axs[i].imshow(im, cmap="gray", vmin=0, vmax=255)
-        axs[i].set_title(pathlib.Path(sys.argv[i + 1]).name)
-        axs[i].set_xticks([])
-        axs[i].set_yticks([])
+    im = open_file(sys.argv[i + 1])
+    axs[i].imshow(im, cmap="gray", vmin=0, vmax=255)
+    axs[i].set_title(pathlib.Path(sys.argv[i + 1]).name)
+    axs[i].set_xticks([])
+    axs[i].set_yticks([])
 
 if nimg % 2 == 1:
     axs[-1].axis("off")
